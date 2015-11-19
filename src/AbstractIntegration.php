bcb5dc4acf18553ecafd8264e802c26046923866
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:30:06 +0100

    Made config factory more flexible.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -1,8 +1,8 @@
 <?php
 
 abstract class Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
-	public function get_settings_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Settings';
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
 	}
 
 	public function get_config_class() {
