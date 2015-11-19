35e1c6f0e226d02510dfdaad6b1adf6a9cc70c02
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:16:09 +0100

    Added settings class.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -9,6 +9,10 @@ abstract class Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration extends P
 		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
 	}
 
+	public function get_settings_class() {
+		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Settings';
+	}
+
 	public function get_gateway_class() {
 		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
 	}
