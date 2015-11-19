fc488d459f6b8631e9cf2e5d486c54fe6a1532c2
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 14:45:56 +0100

    Added support for iDEAL Basic listener.

--- src/AbstractIntegration.php
+++ src/AbstractIntegration.php
@@ -1,6 +1,17 @@
 <?php
 
 abstract class Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
+	public function __construct() {
+		parent::__construct();
+
+		// Actions
+		$function = array( 'Pronamic_WP_Pay_Gateways_IDealBasic_Listener', 'listen' );
+
+		if ( ! has_action( 'wp_loaded', $function ) ) {
+			add_action( 'wp_loaded', $function );
+		}
+	}
+
 	public function get_config_factory_class() {
 		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
 	}
