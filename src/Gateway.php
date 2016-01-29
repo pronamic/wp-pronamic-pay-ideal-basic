ea6acd41e8110982296f3e4d35df83efe3c9a1c3
Remco Tolsma <info@remcotolsma.nl>
2016-01-29 16:19:08 +0100

    Getting ready for version 1.1.2.

--- src/Gateway.php
+++ src/Gateway.php
@@ -3,7 +3,7 @@
 /**
  * Title: iDEAL Basic gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2015
+ * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  * @author Remco Tolsma
  * @since 1.0.0
