e0a35f4bae30962034b63b0d06193577ea7bc999
I <info@remcotolsma.nl>
2016-10-19 10:41:12 +0200

    Getting ready for version 1.1.6.

--- src/Gateway.php
+++ src/Gateway.php
@@ -7,7 +7,7 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.1.5
+ * @version 1.1.6
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_IDealBasic_Gateway extends Pronamic_WP_Pay_Gateway {
