81d38d87a3b56f9d53b681abe6b91b5db59bb646
Reüel van der Steege <reuel@pronamic.nl>
2018-01-05 13:48:29 +0100

    Happy 2018.

--- src/XML/NotificationParser.php
+++ src/XML/NotificationParser.php
@@ -3,7 +3,7 @@
 /**
  * Title: Issuer XML parser
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
