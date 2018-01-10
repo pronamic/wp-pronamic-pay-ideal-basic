47fba33d7b7eed41fdf86a8387b9b4e47553b394
Remco Tolsma <info@remcotolsma.nl>
2018-01-10 15:52:31 +0100

    Addded a not abstract integration class for iDEAL Basic.

---
+++ src/Integration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: Integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2018
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.1.3
+ * @since 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_IDealBasic_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
+	
+}
