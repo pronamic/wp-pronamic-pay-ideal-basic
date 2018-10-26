1ef3ee044d4093494e6f1618dc36a9a87776a58e
Reüel van der Steege <reuel@pronamic.nl>
2018-10-26 14:14:33 +0200

    Update Pronamic Money object for item prices in tests.

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -5,8 +5,11 @@ namespace Pronamic\WordPress\Pay\Gateways\IDealBasic;
 use Pronamic\WordPress\Money\Money;
 
 class ItemTest extends \WP_UnitTestCase {
+	/**
+	 * Test item.
+	 */
 	public function test_item() {
-		$item = new Item( '1', 'Description', 1, 123.50 );
+		$item = new Item( '1', 'Description', 1, new Money( 123.50 ) );
 
 		$this->assertEquals( '1', $item->get_number() );
 		$this->assertEquals( 'Description', $item->get_description() );
--- tests/ItemsTest.php
+++ tests/ItemsTest.php
@@ -2,13 +2,18 @@
 
 namespace Pronamic\WordPress\Pay\Gateways\IDealBasic;
 
+use Pronamic\WordPress\Money\Money;
+
 class ItemsTest extends \WP_UnitTestCase {
+	/**
+	 * Test item.
+	 */
 	public function test_item() {
 		$items = new Items();
 
-		$item1 = new Item( '1', '1 Item of € 1,-', 1, 1 );
-		$item2 = new Item( '2', '1 Item of € 2,-', 1, 2 );
-		$item3 = new Item( '3', '2 Items of € 5,-', 2, 5 );
+		$item1 = new Item( '1', '1 Item of € 1,-', 1, new Money( 1 ) );
+		$item2 = new Item( '2', '1 Item of € 2,-', 1, new Money( 2 ) );
+		$item3 = new Item( '3', '2 Items of € 5,-', 2, new Money( 5 ) );
 
 		$items->add_item( $item1 );
 		$items->add_item( $item2 );
