179276f96287ae23963e077611d18b3bfd1fe197
Reüel van der Steege <reuel@pronamic.nl>
2019-09-27 09:34:42 +0200

    Move class Core\Statuses to Payments\PaymentStatus.

--- tests/src/NotificationTest.php
+++ tests/src/NotificationTest.php
@@ -3,7 +3,7 @@
 namespace Pronamic\WordPress\Pay\Gateways\IDealBasic;
 
 use DateTime;
-use Pronamic\WordPress\Pay\Core\Statuses;
+use Pronamic\WordPress\Pay\Payments\PaymentStatus;
 
 class NotificationTest extends \WP_UnitTestCase {
 	public function test_notification() {
@@ -11,11 +11,11 @@ class NotificationTest extends \WP_UnitTestCase {
 		$notification->set_date( new DateTime() );
 		$notification->set_transaction_id( '1234567890' );
 		$notification->set_purchase_id( '123456' );
-		$notification->set_status( Statuses::SUCCESS );
+		$notification->set_status( PaymentStatus::SUCCESS );
 
 		$this->assertInstanceOf( 'DateTime', $notification->get_date() );
 		$this->assertEquals( '1234567890', $notification->get_transaction_id() );
 		$this->assertEquals( '123456', $notification->get_purchase_id() );
-		$this->assertEquals( Statuses::SUCCESS, $notification->get_status() );
+		$this->assertEquals( PaymentStatus::SUCCESS, $notification->get_status() );
 	}
 }
