87eb46dcdefdcfa154a958221ff3aa28b426f614
Reüel van der Steege <reuel@pronamic.nl>
2021-06-27 00:02:50 +0200

    Replace `Pronamic_WP_Pay_Gateway` with `Core_Gateway` in method documentation.

--- src/Gateway.php
+++ src/Gateway.php
@@ -50,7 +50,7 @@ class Gateway extends Core_Gateway {
 	/**
 	 * Get supported payment methods
 	 *
-	 * @see Pronamic_WP_Pay_Gateway::get_supported_payment_methods()
+	 * @see Core_Gateway::get_supported_payment_methods()
 	 */
 	public function get_supported_payment_methods() {
 		return array(
