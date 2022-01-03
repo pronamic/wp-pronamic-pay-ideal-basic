4efd75d927220efb53ece7348f43ced28e4a79ec
Reüel van der Steege <reuel@pronamic.nl>
2022-01-03 14:05:51 +0100

    Happy 2022.

--- src/Items.php
+++ src/Items.php
@@ -9,7 +9,7 @@ use Pronamic\WordPress\Money\Money;
 /**
  * Title: iDEAL Basic items
  * Description:
- * Copyright: 2005-2021 Pronamic
+ * Copyright: 2005-2022 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
