39b9f60f9a2842b5c344c2936b36bf8c57c9389c
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-13 11:55:44 +0200

    Mark iDEAL payment method as active.

--- src/Gateway.php
+++ src/Gateway.php
@@ -64,7 +64,10 @@ class Gateway extends Core_Gateway {
 		$this->client->set_hash_key( $config->hash_key );
 
 		// Methods.
-		$this->register_payment_method( new PaymentMethod( PaymentMethods::IDEAL ) );
+		$payment_method_ideal =  new PaymentMethod( PaymentMethods::IDEAL );
+		$payment_method_ideal->set_status( 'active' );
+
+		$this->register_payment_method( $payment_method_ideal );
 	}
 
 	/**
